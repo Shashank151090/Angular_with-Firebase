800ce02c02575baa92f2e967a4f6d270af5a66cb
Shashank <shashank151090@gmail.com>
2019-08-09 01:11:34 +0530

    adding new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { AvatarDialogComponent } from './avatar-dialog/avatar-dialog.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { EditUserResolver } from './edit-user/edit-user.resolver';
+import { NewUserComponent } from './new-user/new-user.component';
+import { HomeComponent } from './home/home.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { FirebaseService } from './services/firebase.service';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule, MatSliderModule, MatDialogModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatRadioModule, MatSelectModule, MatOptionModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AvatarDialogComponent,
+    EditUserComponent,
+    NewUserComponent,
+    HomeComponent
+  ],
+  entryComponents: [AvatarDialogComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSliderModule,
+    MatDialogModule,
+    MatButtonModule,  
+  MatMenuModule,  
+  MatToolbarModule,  
+  MatIconModule,  
+  MatCardModule,  
+  MatFormFieldModule,  
+  MatInputModule,  
+  MatDatepickerModule,  
+  MatNativeDateModule,  
+  MatRadioModule,  
+  MatSelectModule,  
+  MatOptionModule,  
+    
+  ],
+  providers: [FirebaseService, EditUserResolver],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDMRHrJxaUJx9ItIhS5TuAC1IhIDv0aInw",
+    authDomain: "demo2-ed900.firebaseapp.com",
+    databaseURL: "https://demo2-ed900.firebaseio.com",
+    projectId: "demo2-ed900",
+    storageBucket: "",
+    messagingSenderId: "990867038869",
+    appId: "1:990867038869:web:aae766e40662ff5b"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
